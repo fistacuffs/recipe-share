81d31291be7b79734373fff5d31a7e9d1a89fe28
Nicholas Weitzel <nicholasweitzel@gmail.com>
2018-12-31 17:25:50 -0500

    Set up file structure for recipe share system.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <link rel="stylesheet" type="text/css" href="styles/style.css" />
+  <title>The Recipe Center</title>
+</head>
+
+<body>
+  <div id="header">
+    <?php include("inc/header.inc.php"); ?>
+  </div>
+  <div id="content">
+    <div id = "nav">
+      <?php include("inc/nav.inc.php"); ?>
+    </div>
+    <div id="main">
+      <?php
+        if (!isset($_REQUEST['content']))
+          include("main.inc.php");
+        else {
+          $content = $_REQUEST['content'];
+          $nextpage = $content . ".inc.php";
+          include($nextpage);
+        } // end if/else
+       ?>
+    </div>
+  </div>
+  <div id="footer">
+    <?php include("inc/footer.inc.php"); ?>
+  </div>
+</body>
+
+</html>
