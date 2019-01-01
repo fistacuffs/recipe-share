fae0c1adff229631b6a631815ff06345b20c0e11
Nicholas Weitzel <nicholasweitzel@gmail.com>
2018-12-31 19:32:32 -0500

    Construct header and footer.

---
+++ inc/footer.inc.php
@@ -0,0 +1,4 @@
+<a href="mailto:nicholasweitzel@gmail.com">
+  <font size="1">Contact Us</font>
+</a>
+<font size="1"> | &copy; <?php echo date("Y"); ?></font>
---
+++ inc/header.inc.php
@@ -0,0 +1,7 @@
+<h1><br>The Recipe Center</h1>
+<h4>
+  <em>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    "nothing like home cookin'"
+  </em>
+</h4>
