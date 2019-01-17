d2d8adc0fd6c8177c091dcb2fe0823c7e337f0be
Nicholas Weitzel <nicholasweitzel@gmail.com>
2019-01-17 13:13:47 -0500

    Add user validation for commenting and adding recipes.

--- inc/newcomment.inc.php
+++ inc/newcomment.inc.php
@@ -1,18 +1,30 @@
 <?php
   $recipeid = $_GET['id'];
 
-  echo "<form action=\"index.php\" method=\"post\">";
-    echo "<h2>Enter your comment</h2>";
-    echo "<textarea rows=\"10\" cols=\"50\" name=\"comment\"></textarea>";
+  if (!isset($_SESSION['valid_recipe_user'])) {
+    echo "<h2>Sorry, you do not have permission to post comments</h2>";
     echo "<br>";
-
-    echo "Submitted by:<input type=\"text\" name=\"poster\">";
+    echo "<a href=\"index.php?content=login\">";
+      echo "Please login to post comments";
+    echo "</a>";
     echo "<br>";
+    echo "<a href=\"index.php?content=showrecipe&id=$recipeid\">";
+      echo "Go back to recipe";
+    echo "</a>";
+  } else {
+    $userid = $_SESSION['valid_recipe_user'];
 
-    echo "<input type=\"hidden\" name=\"recipeid\" value=\"$recipeid\">";
-    echo "<input type=\"hidden\" name=\"content\" value=\"addcomment\">";
-    echo "<br>";
+    echo "<form action=\"index.php\" method=\"post\">";
+      echo "<h2>Enter your comment</h2>";
+      echo "<textarea rows=\"10\" cols=\"50\" name=\"comment\"></textarea>";
+      echo "<br>";
 
-    echo "<input type=\"submit\" value=\"Submit\">";
-  echo "</form>";
+      echo "<input type=\"hidden\" name=\"poster\" value=\"$userid\">";
+      echo "<input type=\"hidden\" name=\"recipeid\" value=\"$recipeid\">";
+      echo "<input type=\"hidden\" name=\"content\" value=\"addcomment\">";
+
+      echo "<br>";
+      echo "<input type=\"submit\" value=\"Submit\">";
+    echo "</form>";
+  }
  ?>
--- inc/newrecipe.inc.php
+++ inc/newrecipe.inc.php
@@ -1,28 +1,40 @@
-<form action="index.php" method="post">
-  <h2>Enter your new recipe</h2>
-  <br>
+<?php
+  if (!isset($_SESSION['valid_recipe_user'])) {
+    echo "<h2>Sorry, you do not have permission to post recipes</h2>";
+    echo "<br>";
+    echo "<a href=\"index.php?content=login\">";
+      echo "Please login to post recipes";
+    echo "</a>";
+    echo "<br>";
+    echo "<a href=\"index.php\">Return to Home</a>";
+  } else {
+    $userid = $_SESSION['valid_recipe_user'];
 
-  Title:
-  <input type="text" size="40" name="title">
-  <br>
+    echo "<form action=\"index.php\" method=\"post\">";
+      echo "<h2>Enter your new recipe</h2>";
+      echo "<br>";
 
-  Poster:
-  <input type="text" size="40" name="poster">
-  </br>
+      echo "Title:";
+      echo "<input type=\"text\" size=\"40\" name=\"title\">";
+      echo "<br>";
 
-  Short Desccription:
-  <br>
-  <textarea rows="5" cols="50" name="shortdesc"></textarea>
-  <br>
+      echo "<input type=\"hidden\" name=\"poster\" value=\"$userid\">";
 
-  <h3>Ingredients (one item per line)</h3>
-  <textarea rows="10" cols="50" name="ingredients"></textarea>
-  <br>
+      echo "Short Desccription:";
+      echo "<br>";
+      echo "<textarea rows=\"5\" cols=\"50\" name=\"shortdesc\"></textarea>";
+      echo "<br>";
 
-  <h3>Directions</h3>
-  <textarea rows="10" cols="50" name="directions"></textarea>
-  <br>
+      echo "<h3>Ingredients (one item per line)</h3>";
+      echo "<textarea rows=\"10\" cols=\"50\" name=\"ingredients\"></textarea>";
+      echo "<br>";
 
-  <input type="submit" value="Submit">
-  <input type="hidden" name="content" value="addrecipe">
-</form>
+      echo "<h3>Directions</h3>";
+      echo "<textarea rows=\"10\" cols=\"50\" name=\"directions\"></textarea>";
+      echo "<br>";
+
+      echo "<input type=\"submit\" value=\"Submit\">";
+      echo "<input type=\"hidden\" name=\"content\" value=\"addrecipe\">";
+    echo "</form>";
+  } // end if/else
+ ?>
